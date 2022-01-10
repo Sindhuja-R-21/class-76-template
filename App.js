fc34d1cbefabe01ffdcee27e884aaf1e664a866b
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-01-10 19:52:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Text, View } from 'react-native';
+
+
+
+
+function App() {
+  return (
+  
+     <View>
+
+     </View>
+
+   
+  );
+}
+
+export default App;
